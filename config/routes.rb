96fc3242b3930fd3208b8803134be157b0c331c1
Dennis Every <dennis@ansyra.com>
2022-06-11 16:22:38 -0400

    Added Like and Retweet buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :tweets
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :tweets do
+    resource :like
+    resource :retweet
+  end
   root "tweets#index"
 end
